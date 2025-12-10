01f4e0d164bd99d953a95c9b7fc6f639ac80a3ed
wairimu-ian <wairimuian20@gmail.com>
2025-12-10 10:06:22 +0300

    changes

---
+++ index.js
@@ -0,0 +1,21 @@
+
+// function add(){ 
+//     let a;
+//     let b;
+//     let answer;
+
+//     if(add(a,b)){
+//           answer = a + b 
+//     };
+// };
+// console.log(add(2,3));
+
+
+// function add(a,b){
+//     return a + b;
+// }
+// console.log(add(2,3));
+
+
+let add = (a,b) => (a+b);
+console.log(add(2,3));
